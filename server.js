93022134b5f8481b6ae187f1a2c93c111f7c7d36
kedusi <kedusi@gmail.com>
2022-07-21 03:41:46 -0500

    404 middleware

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ app.use(bodyParser.json())
 
 const routes = require('./api/routes/steritusRoutes')
 routes(app)
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
